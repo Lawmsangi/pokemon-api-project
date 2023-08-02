5997daea2632e29ff11c711f790f0767838ce176
Lawmsangi <ddarkphoenixx22@gmail.com>
2023-08-02 22:29:43 +0530

    Add gradient

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import getPokemon from './modules/getPokemon';
 import { getLikes } from './modules/like';
 import './style.css';
 
-const pokemonId = [197,150,282,133,300,151,39,1,4, 7, 10,13,16,19,21];
+const pokemonId = [197,150,282,133,300,493,484,39,25,480,481,482,392,1,3, 7, 10,13,16,19,151];
 
 async function fetchPokemons() {
   const pokemons = await Promise.all(
